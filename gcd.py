ac490017fa1a3597080fe7c4dc5df91b1d83efe3
Guillaume Binet <gbin@gootz.net>
2016-03-13 15:49:12 -0700

    Switched to gcloud.

--- gcd.py
+++ gcd.py
@@ -1,13 +1,13 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
+import datetime
 import logging
-from typing import Any, Mapping
-import shelve
+from typing import Any
 import os
 
+from jsonpickle import encode, decode
 from errbot.storage.base import StorageBase, StoragePluginBase
-from googleapiclient.discovery import build
-from oauth2client.client import GoogleCredentials
+from gcloud import datastore
+
 log = logging.getLogger('errbot.storage.gcd')
 
 ACCOUNT_FILE_ENTRY = 'accountfile'
@@ -16,33 +16,39 @@ PROJECT_ENTRY = 'project'
 class CloudDatastore(StorageBase):
     def __init__(self, namespace, project, credentials):
         log.debug('Try to authenticate Google cloud storage on %s with %s' % (project, credentials))
-        self.ds = build('datastore', 'v1beta2', credentials=credentials)
+        self.ds = datastore.Client.from_service_account_json(credentials, project=project, namespace=namespace)
         log.debug('API built %s', self.ds)
-        log.debug('Datasets = %r', self.ds.datasets())
-        ts = self.ds.datasets().beginTransaction(datasetId=project, body={'isolationLevel':'NON_TRANSACTIONAL'}).execute()
-        log.debug('ts = %r', ts['transaction'])
 
+    def _gkey(self, key):
+        return self.ds.key('Errbot', key)
+
+    def _get_all(self):
+        query = self.ds.query(kind='Errbot')
+        return list(query.fetch())
 
     def get(self, key: str) -> Any:
-        return self.shelf[key]
+        resp = self.ds.get(self._gkey(key))
+        if not resp:
+            raise KeyError("%s doesn't exist." % key)
+        return decode(resp['value'])
 
     def remove(self, key: str):
-        if key not in self.shelf:
-            raise KeyError("%s doesn't exist." % key)
-        del self.shelf[key]
+        key = self.ds.key('Errbot', key)
+        self.ds.delete(key)
 
     def set(self, key: str, value: Any) -> None:
-        self.shelf[key] = value
+        ent = datastore.Entity(key=self._gkey(key))
+        ent['value'] = encode(value)
+        self.ds.put(ent)
 
     def len(self):
-        return len(self.shelf)
+        return len(self._get_all())  # TODO: optimize
 
     def keys(self):
-        return self.shelf.keys()
+        return [ent.key.name for ent in self._get_all()]
 
     def close(self) -> None:
-        self.shelf.close()
-        self.shelf = None
+        pass
 
 
 class CloudDataStorePlugin(StoragePluginBase):
@@ -50,11 +56,8 @@ class CloudDataStorePlugin(StoragePluginBase):
         super().__init__(bot_config)
         if PROJECT_ENTRY not in self._storage_config:
             raise Exception('You need to specify a project in your config.py like this: STORAGE_CONFIG={"project":"albator"}')
-        acc = self._storage_config[ACCOUNT_FILE_ENTRY] if ACCOUNT_FILE_ENTRY in self._storage_config else  os.path.join(bot_config.BOT_DATA_DIR, 'servacc.json')
+        self.credentials = self._storage_config[ACCOUNT_FILE_ENTRY] if ACCOUNT_FILE_ENTRY in self._storage_config else os.path.join(bot_config.BOT_DATA_DIR, 'servacc.json')
         self.prj = self._storage_config[PROJECT_ENTRY]
-        self.credentials = GoogleCredentials.from_stream(acc)
-        self.credentials._scopes = 'https://www.googleapis.com/auth/cloud-platform'
 
     def open(self, namespace: str) -> StorageBase:
-        config = self._storage_config
         return CloudDatastore(namespace, self.prj, self.credentials)
